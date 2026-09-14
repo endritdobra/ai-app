5a8ffd912110bcc14a12997db453417756474701
endritdobra <endrit.dobra@gmail.com>
2026-09-14 09:29:23 +0200

    apply consistent formatting across all project files using Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,21 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button.tsx";
+import './App.css';
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button.tsx';
 
 function App() {
-  const [message, setMessage] = useState<string>("Hello World!");
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.text())
-      .then((data) => setMessage(data));
-  }, []);
+   const [message, setMessage] = useState<string>('Hello World!');
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.text())
+         .then((data) => setMessage(data));
+   }, []);
 
-  return (
-    <div className="p-4">
-      <h1 className={"font-bold text-3xl"}>{message}</h1>
-      <Button>Hello</Button>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <h1 className={'font-bold text-3xl'}>{message}</h1>
+         <Button>Hello</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
-import type { Request, Response } from "express";
-import express from "express";
-import dotenv from "dotenv";
+import type { Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req  : Request, res: Response) => {
-    res.send("Hello World2!")
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World2!');
 });
 
-app.get('/api/hello', (req  : Request, res: Response) => {
-    res.send("Hello World3!")
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send('Hello World3!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+   console.log(`Server is running on port ${port}`);
 });
